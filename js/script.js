b8f062ca86ce71afbb84b7831457255247242bbb
leowbrown <68573501+leowbrown@users.noreply.github.com>
2020-08-19 17:29:05 +1200

    moved to one page application

--- js/script.js
+++ js/script.js
@@ -1,12 +1,39 @@
 console.log('js');
 
-$('.ty--msg').hide();
+$('.booking--page').hide();
+$('.contact--page').hide();
 
 $(document).ready(function(){
 
-  $('#sub--button').submit(function(e){
-    e.preventDefault()
-    $('.ty--msg').show();
+  $('#booking--page').click(function(){
+    $('.booking--page').show();
+    $('.home--page').hide();
+    $('.contact--page').hide();
   });
+
+  $('#booking--page--button').click(function(){
+    $('.booking--page').show();
+    $('.home--page').hide();
+    $('.contact--page').hide();
+  });
+
+  $('#contact--page').click(function(){
+    $('.booking--page').hide();
+    $('.home--page').hide();
+    $('.contact--page').show();
+  });
+
+  $('#home--page').click(function(){
+    $('.booking--page').hide();
+    $('.home--page').show();
+    $('.contact--page').hide();
+  });
+
+  $('#home--page--button').click(function(){
+    $('.booking--page').hide();
+    $('.home--page').show();
+    $('.contact--page').hide();
+  });
+
 
 });
