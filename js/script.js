0673a20128915f68b2eaec027cb03ec9b53b80f9
leowbrown <68573501+leowbrown@users.noreply.github.com>
2020-08-19 20:30:17 +1200

    added styling to menu page

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ console.log('js');
 
 $('.booking--page').hide();
 $('.contact--page').hide();
+$('.menu--page').hide();
+// $('.home--page').hide();
 
 $(document).ready(function(){
 
@@ -9,31 +11,43 @@ $(document).ready(function(){
     $('.booking--page').show();
     $('.home--page').hide();
     $('.contact--page').hide();
+    $('.menu--page').hide();
   });
 
   $('#booking--page--button').click(function(){
     $('.booking--page').show();
     $('.home--page').hide();
     $('.contact--page').hide();
+    $('.menu--page').hide();
   });
 
   $('#contact--page').click(function(){
     $('.booking--page').hide();
     $('.home--page').hide();
     $('.contact--page').show();
+    $('.menu--page').hide();
   });
 
   $('#home--page').click(function(){
     $('.booking--page').hide();
     $('.home--page').show();
     $('.contact--page').hide();
+    $('.menu--page').hide();
   });
 
   $('#home--page--button').click(function(){
     $('.booking--page').hide();
     $('.home--page').show();
     $('.contact--page').hide();
+    $('.menu--page').hide();
   });
 
+  $('#menu--page').click(function() {
+    $('.booking--page').hide();
+    $('.home--page').hide();
+    $('.contact--page').hide();
+    $('.menu--page').show();
+
+  })
 
 });
