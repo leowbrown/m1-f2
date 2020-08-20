0618c2eb2e11b4fe4e825c8800c636ba0aea01ed
leowbrown <68573501+leowbrown@users.noreply.github.com>
2020-08-20 20:36:47 +1200

    started working on mobile/tablet version

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+const menuBtn = document.querySelector('.menu--btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    navLinks.classList.toggle("active");
+  // if(!menuOpen) {
+  //   menuBtn.classList.add('open');
+  //   menuOpen = true;
+  // } else {
+  //   menuBtn.classList.remove('open');
+  //   menuOpen = false;
+  // }
+});
+
+
+const navLinks = document.querySelector('.nav--list');
+const links = document.querySelectorAll('nav--link');
+
+
 console.log('js');
 
 $('.booking--page').hide();
@@ -7,11 +25,14 @@ $('.menu--page').hide();
 
 $(document).ready(function(){
 
+
   $('#booking--page').click(function(){
     $('.booking--page').show();
     $('.home--page').hide();
     $('.contact--page').hide();
     $('.menu--page').hide();
+    $( ".nav--list" ).toggleClass( "active" )
+    // $( ".menu--btn" ).toggleClass( "open" );
   });
 
   $('#booking--page--button').click(function(){
@@ -26,6 +47,8 @@ $(document).ready(function(){
     $('.home--page').hide();
     $('.contact--page').show();
     $('.menu--page').hide();
+    $( ".nav--list" ).toggleClass( "active" );
+    // $( ".menu--btn" ).toggleClass( "open" );
   });
 
   $('#home--page').click(function(){
@@ -33,6 +56,8 @@ $(document).ready(function(){
     $('.home--page').show();
     $('.contact--page').hide();
     $('.menu--page').hide();
+    $( ".nav--list" ).toggleClass( "active" );
+    // $( ".menu--btn" ).toggleClass( "open" );
   });
 
   $('#home--page--button').click(function(){
@@ -47,7 +72,9 @@ $(document).ready(function(){
     $('.home--page').hide();
     $('.contact--page').hide();
     $('.menu--page').show();
+    $( ".nav--list" ).toggleClass( "active" );
+    // $( ".menu--btn" ).toggleClass( "open" );
+  });
 
-  })
 
 });
